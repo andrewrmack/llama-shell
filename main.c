2233ddec37a8dc1993857e37268f0c09d6b72dd2
Andrew Mack <andrewrmack@gmail.com>
2015-05-24 23:13:11 -0400

    Added license headers

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+/* llama-shell -- a linux shell */
+
+/* Copyright (C) 2015 Andrew Mack
+ *
+ * This file is part of llama-shell
+ *
+ * llama-shell is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the license, or
+ * (at your option) any later version.
+ *
+ * llama-shell is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have recieved a copy of the GNU General Public License
+ * along with llama-shell. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
